c66207a6beba20629f6eaef63fa5528588237bd1
PAOSTEPH\PAOLITA <pao_st55@hotmail.com>
2018-05-13 16:53:52 -0500

    Loggin actualizado e inicio doctor

---
+++ SIGECMED/src/doctorGUI/CitasDoctorController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doctorGUI;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author PAOLITA
+ */
+public class CitasDoctorController implements Initializable {
+
+    @FXML
+    private JFXButton id_Nombre;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void hacerAlgoDos(ActionEvent event) {
+    }
+    
+}
