f907952139deb5d68fb7bc70b2078319647dd446
Unknown <franizus@live.com>
2018-05-15 10:56:16 -0500

    added jfx button to test

--- SIGECMED/src/views/CalendarApp.java
+++ SIGECMED/src/views/CalendarApp.java
@@ -8,12 +8,17 @@ import com.calendarfx.view.page.DayPage;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import com.jfoenix.controls.JFXButton;
 
 public class CalendarApp extends Application {
 
         @Override
         public void start(Stage primaryStage) throws Exception {
+            
+            BorderPane pane = new BorderPane();
 
             DayPage calendarView = new DayPage();
 
@@ -43,8 +48,14 @@ public class CalendarApp extends Application {
                 updateTimeThread.setPriority(Thread.MIN_PRIORITY);
                 updateTimeThread.setDaemon(true);
                 updateTimeThread.start();
+                
+                JFXButton btn = new JFXButton();
+                btn.textProperty().set("Bienvenido");
+                btn.buttonTypeProperty().set(JFXButton.ButtonType.RAISED);
+                pane.setCenter(calendarView);
+                pane.setBottom(btn);
 
-                Scene scene = new Scene(calendarView);
+                Scene scene = new Scene(pane);
                 primaryStage.setTitle("Calendar");
                 primaryStage.setScene(scene);
                 primaryStage.setWidth(1300);
